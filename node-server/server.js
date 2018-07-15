1f3f438b8ad4f82d22af03fc5aab10c2d09f6601
Marouf Baghdadi <marouf33@gmail.com>
2018-07-15 20:49:21 +0300

    Add firebase to client and server.

--- node-server/server.js
+++ node-server/server.js
@@ -3,6 +3,15 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 var io = require('socket.io')(http);
+var admin = require("firebase-admin");
+
+var serviceAccount = require("private.json");
+var firebaseCredential = require(__dirname + '/private/serviceCredentials.json')
+
+admin.initializeApp({
+  credential: admin.credential.cert(firebaseCredential),
+  databaseURL: "https://beastchat-a6336.firebaseio.com"
+});
 
 io.on('connection', (socket)=>{
   console.log(`Client ${socket.id} is connected!`);
