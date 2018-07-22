74699ba3f57d533f8d9a24cbdb8aa88235910c6b
Marouf <marouf33@gmail.com>
2018-07-22 22:18:28 +0300

    Add missing ViewPagerAdapter

---
+++ app/src/main/java/com/maroufb/beastchat/views/FriendsViewPagerAdapter.java
@@ -0,0 +1,63 @@
+package com.maroufb.beastchat.views;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.maroufb.beastchat.fragments.FindFriendFragment;
+import com.maroufb.beastchat.fragments.FriendRequestFragment;
+import com.maroufb.beastchat.fragments.UserFriendsFragment;
+
+public class FriendsViewPagerAdapter extends FragmentStatePagerAdapter {
+    public FriendsViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+       Fragment returnFragment;
+
+       switch (position){
+           case 0:
+               returnFragment = UserFriendsFragment.newInstance();
+               break;
+           case 1:
+               returnFragment = FriendRequestFragment.newInstance();
+               break;
+           case 2:
+               returnFragment = FindFriendFragment.newInstance();
+               break;
+           default:
+                   return null;
+       }
+       return returnFragment;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        CharSequence title;
+        switch (position){
+            case 0:
+                title = "Friends";
+                break;
+            case 1:
+                title = "Requests";
+                break;
+            case 2:
+                title = "Find Friends";
+                break;
+            default:
+                return null;
+
+
+        }
+        return title;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
